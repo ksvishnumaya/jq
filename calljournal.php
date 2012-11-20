7808464c07deae1ee4f2891f07d4ce6a0a4d0c64
vishnumaya <ksvmaya@yahoo.co.in>
2012-11-20 16:46:26 +0530

    adding new call journal

--- calljournal.php
+++ calljournal.php
@@ -42,7 +42,7 @@
 		
 
 		
-		$( "#button" ).button();
+		$( ".button" ).button();
 		$( "#radioset" ).buttonset();
 		
 
@@ -159,19 +159,25 @@ $q3=mysql_query("select id from user where poc_id='$id';");
 while($volid=mysql_fetch_row($q3))
 {
 
-$q=mysql_query("select * from calljournal where callerid='$id' and calleeid='$volid[0]';");
+$q=mysql_query("select calldate,pledged,raised,id from calljournal where callerid='$id' and calleeid='$volid[0]';");
+
 $q2=mysql_query("select name from user where id='$volid[0]';");
 $volname=mysql_fetch_row($q2);
-//$q2=mysql_query("select name from user where poc_id='$id';");
-
+$q1=mysql_query("select sum(pledged),sum(raised) from calljournal where callerid='$id' and calleeid='$volid[0]';");
+$r1=mysql_fetch_row($q1);
 while($res=mysql_fetch_row($q))
-
 {
 	
-echo "<tr><td>$volname[0]</td><td>$res[1]</td><td>$res[4]</td><td>$res[5]</td></tr>";
+echo "<tr><td>$volname[0]</td>
+<td>$res[0]</td>
+<td>$res[1]</td>
+<td>$res[2]</td>
+<td><form action='editcall.php' method='post'><input type='submit' value='save' class='button'></form></td></tr> ";
 
 }
 
+	
+echo "<tr bgcolor='sky blue'><td>$volname[0]</td><td>Total</td><td>$r1[0]</td><td>$r1[1]</td></tr>";
 echo"<form id='form1' action='callentry.php' method='post'>
 
 	<tr><td><input type='text' value=$volname[0] name='volname'></td>
@@ -179,10 +185,14 @@ echo"<form id='form1' action='callentry.php' method='post'>
 		<td><input type='date' name='calldate'></td>
 		<td><input type='text' name='pledged'></td>
 		<td><input type='raised' name='raised'></td>
-		<td><input type='submit' value='save'></td>
+		<td><input type='submit' value='Edit' class='button'></td>
 	</tr></form>";
 
 }
+
+$q2=mysql_query("select sum(raised), sum(pledged) from calljournal where callerid='$id' and calleeid!=0;");
+$r2=mysql_fetch_row($q2);
+echo "<tr bgcolor='green'><td></td><td>Total</td><td>$r2[1]</td><td>$r2[0]</td></tr>";
 echo "</table>";
  
 
---
+++ editcall.php
@@ -0,0 +1,73 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>jQuery UI Example Page</title>
+	<link href="css/start/jquery-ui-1.9.1.custom.css" rel="stylesheet">
+	<script src="js/jquery-1.8.2.js"></script>
+	<script src="js/jquery-ui-1.9.1.custom.js"></script>
+	<script>
+	$(function() {
+		
+		$( "#accordion" ).accordion();
+		
+
+		
+		var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#autocomplete" ).autocomplete({
+			source: availableTags
+		});
+		$( ".button" ).button();
+		$( "#radioset" ).buttonset();
+		$( "#tabs" ).tabs();
+		$( "#datepicker" ).datepicker({
+			inline: true
+		});
+		
+
+		
+	</script>
+
+	<style>
+	body{
+		font: 62.5% "Trebuchet MS", sans-serif;
+		margin: 50px;
+	}
+	
+
+	</style>
+<?php
+
+echo"<h1>Edit Call Journal</h1>
+<form id='form1' action='editcalljournal.php' method='post'>
+	<table><tr><td>name</td><td><input type='text' name='volname'></td></tr>
+		<tr><td>date</td><td><input type='date' name='calldate'></td></tr>
+		<tr><td>Pledged Amount</td><td><input type='text' name='pledged'></td></tr>
+		<tr><td>Raised Amount</td><td><input type='raised' name='raised'></td></tr>
+		<tr><td><input type='submit' value='save' class='button'></td></tr></table>
+	</form>";
+	?>
+</html>
