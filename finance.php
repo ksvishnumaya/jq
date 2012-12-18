93b0a201ad84558131d1a8e38475a2f79408e766
ksvishnumaya <ksvmaya@yahoo.co.in>
2012-12-18 20:48:17 +0530

    Update finance.php

--- finance.php
+++ finance.php
@@ -4,8 +4,10 @@
 <?php
 include("common.php");
 $q=mysql_query("select id,name from city");
-$r=mysql_fetch_row($q);
+while($r=mysql_fetch_row($q))
+{
 echo "<a href= 'donorlist.php?cityid=$r[0]'>$r[1]</a><br>";
+}
 ?>
 </body
 
