b6089802cd670a135779c282fe3915f914fd1da4
ksvishnumaya <ksvmaya@yahoo.co.in>
2012-12-23 10:52:20 +0530

    Update donorlist.php

--- donorlist.php
+++ donorlist.php
@@ -5,7 +5,7 @@
 <body>
 <table border=2>
 <tr><td>DL Number</td><td>Name</td><td> Amount</td><td> Phone</td><td>Email</td><td>PAN</td><td>Address</td><td>80g</td></tr>
-</table>
+
 <?php
 include("common.php");
 $city=$_GET['cityid'];
@@ -15,5 +15,6 @@ while($r=mysql_fetch_row($q))
 	echo "<tr><td>$r[1]</td><td>$r[4]</td><td>$r[5]</td><td>$r[6]</td><td>$r[7]</td><td>$r[8]</td><td>$r[9]</td></tr>";
 }
 ?>
+</table>
 </body>
 </html>
