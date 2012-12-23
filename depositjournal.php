62f9d2969600d9f50efc21fcad64a146b16d8db4
ksvishnumaya <ksvmaya@yahoo.co.in>
2012-12-23 10:54:39 +0530

    Update depositjournal.php

--- depositjournal.php
+++ depositjournal.php
@@ -1,11 +1,11 @@
 <?php
 include("common.php");
-echo"<table border=2><tr><td>City </td> <td>Date</td><td>Amount</td></tr>";
+echo"<table border=2><tr><td>City </td> <td>Amount</td><td>Date</td></tr>";
 $q=mysql_query("select * from deposit ORDER BY cityid");
 while($r=mysql_fetch_row($q))
 {
 	$q1=mysql_query("select name from city where id= '$r[1]';");
-	$r1=mysql_fetch_row('$q1');
+	$r1=mysql_fetch_row($q1);
 echo "<tr><td>$r1[0]</td><td>$r[1]</td><td>$r[2]</td></tr>";
 
 }
