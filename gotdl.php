8c2701f67a0a301532f504d46f98ecc99f6c680a
Binny V A <binnyva@gmail.com>
2012-12-29 08:42:10 +0530

    Fixed the recived issue

--- gotdl.php
+++ gotdl.php
@@ -1,13 +1,6 @@
 <?php
-//echo "reached gotdl";
-$rowid=2;
-//$_GET['id'];
 require('common.php');
-$q1=mysql_query("select city_id from donor where id=$rowid;");
-$r1=mysql_fetch_row($q1);
-echo $r1[0];
-setcookie('city',$r1[0]);
-echo $rowid;
+$rowid = $_GET['id'];
 $q=mysql_query("UPDATE  donor SET  gotdl=1 WHERE  id=$rowid;") or die("errorified");
 
 include("donorlist.php");
