a4aca97c46c5226c535e3380c817244bfc0fe4af
Binny V A <binnyva@gmail.com>
2013-01-07 23:49:52 +0530

    Bug fix

--- depositentry.php
+++ depositentry.php
@@ -1,8 +1,7 @@
 <?php
 include("common.php");
-$cityid=$_GET['id'];
 $amount=$_POST['amount'];
 $date=$_POST['date'];
-$q=mysql_query("insert into deposit(cityid,date,amount) values('$cityid','$date',$amount);") or die("could not enter. Enter once again");
+$q=mysql_query("insert into deposit(cityid,date,amount) values('$city_id','$date',$amount);") or die("could not enter. Enter once again");
 include("cityfinance.php");
 ?>
